e4b416c978f4ce694032dad7cf47187401317614
Dvir-Segal <dvirsegal96@gmail.com>
2021-02-23 15:24:16 -0500

    errrrrrrorrrr

--- sniffAndSpoof1.4.py
+++ sniffAndSpoof1.4.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-from scapy.all import*
+from scapy.all import *
 from scapy.layers.inet import IP, ICMP
 
 
 def print_pkt(pkt):
-   #pkt.show
-    a = IP(dst=pkt[IP].src)
-    a.src = pkt[IP].dst
-    a.ttl = 1
-    b = ICMP()
-    b.type = 0
-    send(a / b)
+    # pkt.show
+    if (pkt[ICMP].type == 8):
+        a = IP(dst=pkt[IP].src)
+        a.src = pkt[IP].dst
+        b = ICMP()
+        send(a / b)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     intface = ['enp0s3']
-    pkt = sniff(iface= intface, filter= 'icmp', prn= print_pkt)
+    pkt = sniff(iface=intface, filter='icmp', prn=print_pkt)
